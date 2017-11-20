1969c271e17ee9cae5cff8badc675f07fcf0c64d
Sahil Jain <sahiljainpkl199724@gmail.com>
2017-11-20 17:11:46 +0530

    Added navbar scroll for header

--- js/main.js
+++ js/main.js
@@ -218,3 +218,20 @@ $(document).ready(() => {
   });
 
 });
+var heroShinker = function() {
+    var hero = $('.hero-nav'),
+        heroHeight = $('.hero-nav').outerHeight(true);
+        $(hero).parent().css('padding-top', heroHeight);
+    $(window).scroll(function() {
+        var scrollOffset = $(window).scrollTop();
+        if (scrollOffset < heroHeight) {
+            $(hero).css('height', (heroHeight - scrollOffset));
+        }
+        if (scrollOffset > (heroHeight - 415)) {
+            hero.addClass('fixme');
+        } else {
+            hero.removeClass('fixme');
+        };
+    });
+}
+heroShinker();
